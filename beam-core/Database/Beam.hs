e47e4d30774e19ffaf70045638739f20f5414a76
Ollie Charles <ollie@ocharles.org.uk>
2018-04-06 14:15:41 +0100

    Minimising changes against master

--- beam-core/Database/Beam.hs
+++ beam-core/Database/Beam.hs
@@ -11,6 +11,8 @@
 module Database.Beam
      ( module Database.Beam.Query
      , module Database.Beam.Schema
+     , MonadBeam(withDatabase, withDatabaseDebug)
+     , FromBackendRow(..)
 
        -- * Re-exports
      , MonadIO(..), Typeable
@@ -18,6 +20,7 @@ module Database.Beam
 
 import Database.Beam.Query
 import Database.Beam.Schema
+import Database.Beam.Backend
 
 import Control.Monad.Identity
 import Control.Monad.IO.Class (MonadIO(..))
