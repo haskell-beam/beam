b6d6218ed618eb537b87a7aa70b77d8fd5a62c36
Ollie Charles <ollie@ocharles.org.uk>
2018-04-06 16:13:48 +0100

    Remove outdated TODO

--- beam-core/Database/Beam/Query/Types.hs
+++ beam-core/Database/Beam/Query/Types.hs
@@ -53,6 +53,5 @@ type instance QExprToField (a, b, c, d, e, f, g, h) =
   , QExprToField e, QExprToField f, QExprToField g, QExprToField h)
 type instance QExprToField (Vector n a) = Vector n (QExprToField a)
 
--- TODO ocharles Move to beam-backend
 buildSqlQuery :: Projectible ExpressionSyntax a => TablePrefix -> Q db s a -> SelectSyntax
 buildSqlQuery = buildSql92Query' True
