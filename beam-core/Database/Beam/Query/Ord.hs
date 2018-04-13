ba7c05a743d1924588a436d34c741a949adb2cdd
Brian Jones <brian@uncannyworks.com>
2018-04-13 13:14:27 +0900

    Fix beam postgres 0.7

--- beam-core/Database/Beam/Query/Ord.hs
+++ beam-core/Database/Beam/Query/Ord.hs
@@ -57,6 +57,7 @@ import Data.Proxy
 import Data.Kind
 import Data.Word
 import Data.Int
+import Data.Tagged
 import Data.Text (Text)
 import Data.Time (UTCTime, LocalTime, Day, TimeOfDay)
 
@@ -386,6 +387,8 @@ instance HasSqlEqualityCheck Expression UTCTime
 instance HasSqlEqualityCheck Expression LocalTime
 instance HasSqlEqualityCheck Expression Day
 instance HasSqlEqualityCheck Expression TimeOfDay
+instance HasSqlEqualityCheck Expression a =>
+  HasSqlEqualityCheck Expression (Tagged t a)
 
 instance HasSqlQuantifiedEqualityCheck Expression Text
 instance HasSqlQuantifiedEqualityCheck Expression Integer
@@ -406,6 +409,8 @@ instance HasSqlQuantifiedEqualityCheck Expression UTCTime
 instance HasSqlQuantifiedEqualityCheck Expression LocalTime
 instance HasSqlQuantifiedEqualityCheck Expression Day
 instance HasSqlQuantifiedEqualityCheck Expression TimeOfDay
+instance HasSqlQuantifiedEqualityCheck Expression a =>
+  HasSqlQuantifiedEqualityCheck Expression (Tagged t a)
 
 instance HasSqlEqualityCheck SqlSyntaxBuilder Text
 instance HasSqlEqualityCheck SqlSyntaxBuilder Integer
@@ -426,6 +431,8 @@ instance HasSqlEqualityCheck SqlSyntaxBuilder UTCTime
 instance HasSqlEqualityCheck SqlSyntaxBuilder LocalTime
 instance HasSqlEqualityCheck SqlSyntaxBuilder Day
 instance HasSqlEqualityCheck SqlSyntaxBuilder TimeOfDay
+instance HasSqlEqualityCheck SqlSyntaxBuilder a =>
+  HasSqlEqualityCheck SqlSyntaxBuilder (Tagged t a)
 
 instance HasSqlQuantifiedEqualityCheck SqlSyntaxBuilder Text
 instance HasSqlQuantifiedEqualityCheck SqlSyntaxBuilder Integer
@@ -446,3 +453,5 @@ instance HasSqlQuantifiedEqualityCheck SqlSyntaxBuilder UTCTime
 instance HasSqlQuantifiedEqualityCheck SqlSyntaxBuilder LocalTime
 instance HasSqlQuantifiedEqualityCheck SqlSyntaxBuilder Day
 instance HasSqlQuantifiedEqualityCheck SqlSyntaxBuilder TimeOfDay
+instance HasSqlQuantifiedEqualityCheck SqlSyntaxBuilder a =>
+  HasSqlQuantifiedEqualityCheck SqlSyntaxBuilder (Tagged t a)
