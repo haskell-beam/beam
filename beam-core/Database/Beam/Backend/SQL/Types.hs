eeaf2f847e91df70de8fd9c7dc99c7c4612bb436
Maciej Kazulak <kazulakm@gmail.com>
2018-07-26 00:35:00 +0200

    Use free applicative for FromBackendRow

--- beam-core/Database/Beam/Backend/SQL/Types.hs
+++ beam-core/Database/Beam/Backend/SQL/Types.hs
@@ -5,7 +5,6 @@ import Database.Beam.Backend.Types
 
 import qualified Data.Aeson as Json
 import           Data.Bits
-import           Data.Proxy
 
 class ( BeamBackend be ) =>
       BeamSqlBackend be where
@@ -18,8 +17,7 @@ newtype SqlBitString = SqlBitString Integer
 newtype SqlSerial a = SqlSerial { unSerial :: a }
   deriving (Show, Read, Eq, Ord, Num, Integral, Real, Enum)
 instance FromBackendRow be x => FromBackendRow be (SqlSerial x) where
-  fromBackendRow = SqlSerial <$> fromBackendRow
-  rowRep be _ = rowRep be (Proxy @x)
+  fromBackendRow = fmap SqlSerial <$> fromBackendRow
 instance Json.FromJSON a => Json.FromJSON (SqlSerial a) where
   parseJSON a = SqlSerial <$> Json.parseJSON a
 instance Json.ToJSON a => Json.ToJSON (SqlSerial a) where
--- beam-core/Database/Beam/Backend/Types.hs
+++ beam-core/Database/Beam/Backend/Types.hs
@@ -6,16 +6,16 @@
 module Database.Beam.Backend.Types
   ( BeamBackend(..)
 
-  , FromBackendRowF(..), FromBackendRowM
-  , parseOneField, peekField, checkNextNNull
+  , FromBackendRowF(..), FromBackendRowA
+  , parseOneField, peekField
 
   , FromBackendRow(..)
-
-  , AnyField(..)
+  , valuesNeeded
 
   , Exposed, Nullable ) where
 
-import           Control.Monad.Free.Church
+import           Control.Applicative
+import           Control.Applicative.Free
 import           Control.Monad.Identity
 import           Data.Tagged
 import           Data.Vector.Sized (Vector)
@@ -33,35 +33,25 @@ class BeamBackend be where
   type BackendFromField be :: * -> Constraint
 
 data FromBackendRowF be f where
-  ParseOneField :: BackendFromField be a => (a -> f) -> FromBackendRowF be f
+  ParseOneField :: BackendFromField be a => (Maybe a -> f) -> FromBackendRowF be f
   PeekField :: BackendFromField be a => (Maybe a -> f) -> FromBackendRowF be f
-  CheckNextNNull :: Int -> (Bool -> f) -> FromBackendRowF be f
 deriving instance Functor (FromBackendRowF be)
-type FromBackendRowM be = F (FromBackendRowF be)
+type FromBackendRowA be = Ap (FromBackendRowF be)
 
-parseOneField :: BackendFromField be a => FromBackendRowM be a
-parseOneField = liftF (ParseOneField id)
+parseOneField :: BackendFromField be a => FromBackendRowA be (Maybe a)
+parseOneField = liftAp (ParseOneField id)
 
-peekField :: BackendFromField be a => FromBackendRowM be (Maybe a)
-peekField = liftF (PeekField id)
-
-checkNextNNull :: Int -> FromBackendRowM be Bool
-checkNextNNull n = liftF (CheckNextNNull n id)
-
-data AnyField be = forall a. (BackendFromField be a) => AF (Proxy a)
+peekField :: BackendFromField be a => FromBackendRowA be (Maybe a)
+peekField = liftAp (PeekField id)
 
 class BeamBackend be => FromBackendRow be a where
-  fromBackendRow :: FromBackendRowM be a
-  default fromBackendRow :: BackendFromField be a => FromBackendRowM be a
+  fromBackendRow :: FromBackendRowA be (Maybe a)
+  default fromBackendRow :: (BackendFromField be a) => FromBackendRowA be (Maybe a)
   fromBackendRow = parseOneField
 
-  rowRep :: Proxy be -> Proxy a -> [AnyField be]
-  default rowRep :: BackendFromField be a => Proxy be -> Proxy a -> [AnyField be]
-  rowRep _ _ = [AF (Proxy @a)]
-
-valuesNeeded :: FromBackendRow be a => Proxy be -> Proxy a -> Int
-valuesNeeded be a = length $ rowRep be a
-
+valuesNeeded :: FromBackendRowA be a -> Int
+valuesNeeded (Pure _) = 0
+valuesNeeded (Ap _ a) = 1 + valuesNeeded a
 
 -- | newtype mainly used to inspect tho tag structure of a particular
 --   'Beamable'. Prevents overlapping instances in some case. Usually not used
@@ -79,103 +69,97 @@ data Exposed x
 data Nullable (c :: * -> *) x
 
 class GFromBackendRow be (exposed :: * -> *) rep where
-  gFromBackendRow :: Proxy exposed -> FromBackendRowM be (rep ())
-  gRowRep :: Proxy be -> Proxy exposed -> Proxy rep -> [AnyField be]
-instance GFromBackendRow be e p => GFromBackendRow be (M1 t f e) (M1 t f p) where
-  gFromBackendRow _ = M1 <$> gFromBackendRow (Proxy @e)
-  gRowRep be _ _ = gRowRep be (Proxy @e) (Proxy @p)
+  gFromBackendRow :: Proxy exposed -> FromBackendRowA be (Maybe (rep ()))
 instance GFromBackendRow be e U1 where
-  gFromBackendRow _ = pure U1
-  gRowRep _ _ _ = []
-instance (GFromBackendRow be aExp a, GFromBackendRow be bExp b) => GFromBackendRow be (aExp :*: bExp) (a :*: b) where
-  gFromBackendRow _ = (:*:) <$> gFromBackendRow (Proxy @aExp) <*> gFromBackendRow (Proxy @bExp)
-  gRowRep be _ _ = gRowRep be (Proxy @aExp) (Proxy @a) ++ gRowRep be (Proxy @bExp) (Proxy @b)
+  gFromBackendRow _ = pure (Just U1)
+instance GFromBackendRow be e p => GFromBackendRow be (M1 t f e) (M1 t f p) where
+  gFromBackendRow _ = fmap M1 <$> gFromBackendRow (Proxy @e)
+instance (GFromBackendRow be ea a, GFromBackendRow be eb b) => GFromBackendRow be (ea :*: eb) (a :*: b) where
+  gFromBackendRow _ = liftA2 (:*:) <$> gFromBackendRow (Proxy @ea) <*> gFromBackendRow (Proxy @eb)
 instance FromBackendRow be x => GFromBackendRow be (K1 R (Exposed x)) (K1 R x) where
-  gFromBackendRow _ = K1 <$> fromBackendRow
-  gRowRep be _ _ = rowRep be (Proxy @x)
+  gFromBackendRow _ = fmap K1 <$> fromBackendRow
 instance FromBackendRow be (t Identity) => GFromBackendRow be (K1 R (t Exposed)) (K1 R (t Identity)) where
-  gFromBackendRow _ = K1 <$> fromBackendRow
-  gRowRep be _ _ = rowRep be (Proxy @(t Identity))
+  gFromBackendRow _ = fmap K1 <$> fromBackendRow
 instance FromBackendRow be (t (Nullable Identity)) => GFromBackendRow be (K1 R (t (Nullable Exposed))) (K1 R (t (Nullable Identity))) where
-  gFromBackendRow _ = K1 <$> fromBackendRow
-  gRowRep be _ _ = rowRep be (Proxy @(t (Nullable Identity)))
+  gFromBackendRow _ = fmap K1 <$> fromBackendRow
+
+
 instance BeamBackend be => FromBackendRow be () where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep ()))
-  rowRep _ _ = []
+  fromBackendRow = pure (Just ())
 
 instance ( BeamBackend be, KnownNat n, FromBackendRow be a ) => FromBackendRow be (Vector n a) where
-  fromBackendRow = Vector.replicateM fromBackendRow
-  rowRep be a = concat $ replicate (fromIntegral $ natVal (Proxy @n)) (rowRep be a)
+  fromBackendRow = fmap sequenceA $ sequenceA $ Vector.replicate fromBackendRow
 
 instance ( BeamBackend be, FromBackendRow be a, FromBackendRow be b ) =>
   FromBackendRow be (a, b) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (Exposed a, Exposed b)))
-  rowRep be _ = rowRep be (Proxy @a) ++ rowRep be (Proxy @b)
+  fromBackendRow = liftA2 (,) <$> fromBackendRow <*> fromBackendRow
+
 instance ( BeamBackend be, FromBackendRow be a, FromBackendRow be b, FromBackendRow be c ) =>
   FromBackendRow be (a, b, c) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (Exposed a, Exposed b, Exposed c)))
-  rowRep be _ = concat [rowRep be (Proxy @a), rowRep be (Proxy @b), rowRep be (Proxy @c)]
-instance ( BeamBackend be
-         , FromBackendRow be a, FromBackendRow be b, FromBackendRow be c
-         , FromBackendRow be d ) =>
+  fromBackendRow = liftA3 (,,) <$> fromBackendRow <*> fromBackendRow <*> fromBackendRow
+
+instance ( BeamBackend be, FromBackendRow be a, FromBackendRow be b, FromBackendRow be c, FromBackendRow be d ) =>
   FromBackendRow be (a, b, c, d) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (Exposed a, Exposed b, Exposed c, Exposed d)))
-  rowRep be _ = concat [rowRep be (Proxy @a), rowRep be (Proxy @b), rowRep be (Proxy @c), rowRep be (Proxy @d)]
-instance ( BeamBackend be
-         , FromBackendRow be a, FromBackendRow be b, FromBackendRow be c
-         , FromBackendRow be d, FromBackendRow be e ) =>
+  fromBackendRow = liftA4 (,,,) <$> fromBackendRow <*> fromBackendRow <*> fromBackendRow <*> fromBackendRow
+
+instance ( BeamBackend be, FromBackendRow be a, FromBackendRow be b, FromBackendRow be c, FromBackendRow be d
+                         , FromBackendRow be e ) =>
   FromBackendRow be (a, b, c, d, e) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (Exposed a, Exposed b, Exposed c, Exposed d, Exposed e)))
-  rowRep be _ = concat [ rowRep be (Proxy @a), rowRep be (Proxy @b), rowRep be (Proxy @c), rowRep be (Proxy @d)
-                       , rowRep be (Proxy @e)
-                       ]
-instance ( BeamBackend be
-         , FromBackendRow be a, FromBackendRow be b, FromBackendRow be c
-         , FromBackendRow be d, FromBackendRow be e, FromBackendRow be f ) =>
+  fromBackendRow = liftA5 (,,,,) <$> fromBackendRow <*> fromBackendRow <*> fromBackendRow <*> fromBackendRow
+                                 <*> fromBackendRow
+
+instance ( BeamBackend be, FromBackendRow be a, FromBackendRow be b, FromBackendRow be c, FromBackendRow be d
+                         , FromBackendRow be e, FromBackendRow be f ) =>
   FromBackendRow be (a, b, c, d, e, f) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (Exposed a, Exposed b, Exposed c, Exposed d, Exposed e, Exposed f)))
-  rowRep be _ = concat [ rowRep be (Proxy @a), rowRep be (Proxy @b), rowRep be (Proxy @c), rowRep be (Proxy @d)
-                       , rowRep be (Proxy @e), rowRep be (Proxy @f) ]
-instance ( BeamBackend be
-         , FromBackendRow be a, FromBackendRow be b, FromBackendRow be c
-         , FromBackendRow be d, FromBackendRow be e, FromBackendRow be f
-         , FromBackendRow be g ) =>
+  fromBackendRow = liftA6 (,,,,,) <$> fromBackendRow <*> fromBackendRow <*> fromBackendRow <*> fromBackendRow
+                                  <*> fromBackendRow <*> fromBackendRow
+
+instance ( BeamBackend be, FromBackendRow be a, FromBackendRow be b, FromBackendRow be c, FromBackendRow be d
+                         , FromBackendRow be e, FromBackendRow be f, FromBackendRow be g ) =>
   FromBackendRow be (a, b, c, d, e, f, g) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (Exposed a, Exposed b, Exposed c, Exposed d, Exposed e, Exposed f, Exposed g)))
-  rowRep be _ = concat [ rowRep be (Proxy @a), rowRep be (Proxy @b), rowRep be (Proxy @c), rowRep be (Proxy @d)
-                       , rowRep be (Proxy @e), rowRep be (Proxy @f), rowRep be (Proxy @g) ]
-instance ( BeamBackend be
-         , FromBackendRow be a, FromBackendRow be b, FromBackendRow be c
-         , FromBackendRow be d, FromBackendRow be e, FromBackendRow be f
-         , FromBackendRow be g, FromBackendRow be h ) =>
+  fromBackendRow = liftA7 (,,,,,,) <$> fromBackendRow <*> fromBackendRow <*> fromBackendRow <*> fromBackendRow
+                                   <*> fromBackendRow <*> fromBackendRow <*> fromBackendRow
+
+instance ( BeamBackend be, FromBackendRow be a, FromBackendRow be b, FromBackendRow be c, FromBackendRow be d
+                         , FromBackendRow be e, FromBackendRow be f, FromBackendRow be g, FromBackendRow be h ) =>
   FromBackendRow be (a, b, c, d, e, f, g, h) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (Exposed a, Exposed b, Exposed c, Exposed d, Exposed e, Exposed f, Exposed g, Exposed h)))
-  rowRep be _ = concat [ rowRep be (Proxy @a), rowRep be (Proxy @b), rowRep be (Proxy @c), rowRep be (Proxy @d)
-                       , rowRep be (Proxy @e), rowRep be (Proxy @f), rowRep be (Proxy @g), rowRep be (Proxy @h) ]
+  fromBackendRow = liftA8 (,,,,,,,) <$> fromBackendRow <*> fromBackendRow <*> fromBackendRow <*> fromBackendRow
+                                    <*> fromBackendRow <*> fromBackendRow <*> fromBackendRow <*> fromBackendRow
+
+instance FromBackendRow be a => FromBackendRow be (Maybe a) where
+  fromBackendRow = fmap f (fromBackendRow :: FromBackendRowA be (Maybe a))
+    where
+      f :: Maybe a -> Maybe (Maybe a)
+      f (Just a) = Just (Just a)
+      f Nothing = Just Nothing
 
 instance ( BeamBackend be, Generic (tbl Identity), Generic (tbl Exposed)
          , GFromBackendRow be (Rep (tbl Exposed)) (Rep (tbl Identity))) =>
-
     FromBackendRow be (tbl Identity) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (tbl Exposed)))
-  rowRep be _ = gRowRep be (Proxy @(Rep (tbl Exposed))) (Proxy @(Rep (tbl Identity)))
+  fromBackendRow = fmap to <$> gFromBackendRow (Proxy @(Rep (tbl Exposed)))
+
 instance ( BeamBackend be, Generic (tbl (Nullable Identity)), Generic (tbl (Nullable Exposed))
          , GFromBackendRow be (Rep (tbl (Nullable Exposed))) (Rep (tbl (Nullable Identity)))) =>
-
     FromBackendRow be (tbl (Nullable Identity)) where
-  fromBackendRow = to <$> gFromBackendRow (Proxy @(Rep (tbl (Nullable Exposed))))
-  rowRep be _ = gRowRep be (Proxy @(Rep (tbl (Nullable Exposed)))) (Proxy @(Rep (tbl (Nullable Identity))))
-
-instance FromBackendRow be x => FromBackendRow be (Maybe x) where
-  fromBackendRow =
-    do isNull <- checkNextNNull (valuesNeeded (Proxy @be) (Proxy @(Maybe x)))
-       if isNull then pure Nothing else Just <$> fromBackendRow
-  rowRep be _ = rowRep be (Proxy @x)
-
-deriving instance Generic (a, b, c, d, e, f, g, h)
+  fromBackendRow = fmap to <$> gFromBackendRow (Proxy @(Rep (tbl (Nullable Exposed))))
 
 -- Tagged
 
 instance (BeamBackend be, FromBackendRow be t) => FromBackendRow be (Tagged tag t) where
-  fromBackendRow = Tagged <$> fromBackendRow
-  rowRep be _ = rowRep be (Proxy @t)
+  fromBackendRow = fmap Tagged <$> fromBackendRow
+
+liftA4 :: Applicative f => (a1 -> a2 -> a3 -> a4 -> b) -> f a1 -> f a2 -> f a3 -> f a4 -> f b
+liftA4 f a1 a2 a3 a4 = f <$> a1 <*> a2 <*> a3 <*> a4
+
+liftA5 :: Applicative f => (a1 -> a2 -> a3 -> a4 -> a5 -> b) -> f a1 -> f a2 -> f a3 -> f a4 -> f a5 -> f b
+liftA5 f a1 a2 a3 a4 a5 = f <$> a1 <*> a2 <*> a3 <*> a4 <*> a5
+
+liftA6 :: Applicative f => (a1 -> a2 -> a3 -> a4 -> a5 -> a6 -> b) -> f a1 -> f a2 -> f a3 -> f a4 -> f a5 -> f a6 -> f b
+liftA6 f a1 a2 a3 a4 a5 a6 = f <$> a1 <*> a2 <*> a3 <*> a4 <*> a5 <*> a6
+
+liftA7 :: Applicative f => (a1 -> a2 -> a3 -> a4 -> a5 -> a6 -> a7 -> b) -> f a1 -> f a2 -> f a3 -> f a4 -> f a5 -> f a6 -> f a7 -> f b
+liftA7 f a1 a2 a3 a4 a5 a6 a7 = f <$> a1 <*> a2 <*> a3 <*> a4 <*> a5 <*> a6 <*> a7
+
+liftA8 :: Applicative f => (a1 -> a2 -> a3 -> a4 -> a5 -> a6 -> a7 -> a8 -> b) -> f a1 -> f a2 -> f a3 -> f a4 -> f a5 -> f a6 -> f a7 -> f a8 -> f b
+liftA8 f a1 a2 a3 a4 a5 a6 a7 a8 = f <$> a1 <*> a2 <*> a3 <*> a4 <*> a5 <*> a6 <*> a7 <*> a8
+
