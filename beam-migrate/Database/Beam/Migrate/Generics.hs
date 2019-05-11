1d58f21485e5b95e1edb662d3de26f5d3572289d
Ryan Trinkle <ryan.trinkle@gmail.com>
2019-05-11 15:01:48 -0400

    Allow databases to be nested

--- beam-migrate/Database/Beam/Migrate/Generics.hs
+++ beam-migrate/Database/Beam/Migrate/Generics.hs
@@ -14,23 +14,5 @@ module Database.Beam.Migrate.Generics
  , HasNullableConstraint, NullableStatus
  ) where
 
-import Database.Beam.Migrate.Types
 import Database.Beam.Migrate.Generics.Tables
 import Database.Beam.Migrate.Generics.Types
-
-import Data.Proxy
-
-import GHC.Generics
-
--- | Produce a checked database for the given Haskell database type
---
--- See <http://tathougies.github.io/beam/schema-guide/migrations/ the manual>
--- for more information on the defaults.
-defaultMigratableDbSettings
-  :: forall be db.
-   ( Generic (CheckedDatabaseSettings be db)
-   , GAutoMigratableDb be (Rep (CheckedDatabaseSettings be db)) )
-  => CheckedDatabaseSettings be db
-defaultMigratableDbSettings =
-  to (defaultMigratableDbSettings' (Proxy @be) :: Rep (CheckedDatabaseSettings be db) ())
-
