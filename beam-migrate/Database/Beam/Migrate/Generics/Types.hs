403379763aa5e68c3122c710c49b773d104a5ac7
Ryan Trinkle <ryan.trinkle@gmail.com>
2019-05-11 15:01:48 -0400

    Allow databases to be nested

--- beam-migrate/Database/Beam/Migrate/Generics/Types.hs
+++ beam-migrate/Database/Beam/Migrate/Generics/Types.hs
@@ -2,7 +2,9 @@
 module Database.Beam.Migrate.Generics.Types where
 
 import           Database.Beam.Migrate.Types
+import           Database.Beam.Schema.Tables
 
+import           Data.Functor.Identity
 import           Data.Proxy
 import qualified Data.Text as T
 
@@ -27,3 +29,33 @@ instance ( Selector f, IsCheckedDatabaseEntity be x
 
   defaultMigratableDbSettings' _ = M1 (K1 (CheckedDatabaseEntity (checkedDbEntityAuto name) []))
     where name = T.pack (selName (undefined :: S1 f (Rec0 (CheckedDatabaseEntity be db x)) ()))
+
+instance ( Selector f
+         , Generic (CheckedDatabaseSettings be innerDb)
+         , GAutoMigratableDb be (Rep (CheckedDatabaseSettings be innerDb))
+         , Database be innerDb
+         ) =>
+  GAutoMigratableDb be (S1 f (Rec0 (innerDb (CheckedDatabaseEntity be outerDb)))) where
+
+  defaultMigratableDbSettings' _ = M1 (K1 (runIdentity $ zipTables (Proxy :: Proxy be) (\_ -> pure . changeCheckedDatabaseEntityDb @innerDb @outerDb @be) settings settings :: innerDb (CheckedDatabaseEntity be outerDb))) --TODO: Rename inner tables based on field name
+    where -- name = T.pack (selName (undefined :: S1 f (Rec0 (innerDb (CheckedDatabaseEntity be outerDb))) ()))
+          settings :: CheckedDatabaseSettings be innerDb
+          settings = defaultMigratableDbSettings
+
+changeCheckedDatabaseEntityDb
+  :: forall db1 db2 be entityType
+  .  CheckedDatabaseEntity be db1 entityType
+  -> CheckedDatabaseEntity be db2 entityType
+changeCheckedDatabaseEntityDb (CheckedDatabaseEntity a b) = CheckedDatabaseEntity a b
+
+-- | Produce a checked database for the given Haskell database type
+--
+-- See <http://tathougies.github.io/beam/schema-guide/migrations/ the manual>
+-- for more information on the defaults.
+defaultMigratableDbSettings
+  :: forall be db.
+   ( Generic (CheckedDatabaseSettings be db)
+   , GAutoMigratableDb be (Rep (CheckedDatabaseSettings be db)) )
+  => CheckedDatabaseSettings be db
+defaultMigratableDbSettings =
+  to (defaultMigratableDbSettings' (Proxy @be) :: Rep (CheckedDatabaseSettings be db) ())
