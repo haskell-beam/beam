7c98b9d23e7e59899218c8910e91d91eadd829cd
Ryan Trinkle <ryan@trinkle.org>
2020-02-26 11:55:15 -0500

    Add JSONKey instances for SqlSerial

--- beam-core/Database/Beam/Backend/SQL/Types.hs
+++ beam-core/Database/Beam/Backend/SQL/Types.hs
@@ -3,6 +3,7 @@ module Database.Beam.Backend.SQL.Types where
 
 import qualified Data.Aeson as Json
 import           Data.Bits
+import           Data.Functor.Contravariant (contramap)
 
 data SqlNull = SqlNull
   deriving (Show, Eq, Ord, Bounded, Enum)
@@ -17,4 +18,9 @@ instance Json.FromJSON a => Json.FromJSON (SqlSerial a) where
 instance Json.ToJSON a => Json.ToJSON (SqlSerial a) where
   toJSON (SqlSerial a) = Json.toJSON a
   toEncoding (SqlSerial a) = Json.toEncoding a
-
+instance Json.FromJSONKey a => Json.FromJSONKey (SqlSerial a) where
+  fromJSONKey = SqlSerial <$> Json.fromJSONKey
+  fromJSONKeyList = fmap SqlSerial <$> Json.fromJSONKeyList
+instance Json.ToJSONKey a => Json.ToJSONKey (SqlSerial a) where
+  toJSONKey = contramap unSerial Json.toJSONKey
+  toJSONKeyList = contramap (fmap unSerial) Json.toJSONKeyList
