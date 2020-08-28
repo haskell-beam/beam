02d8a8262b83b8cc27aa4e6b6c2dcf9084da5a78
Travis Athougies <travis@athougies.net>
2020-08-28 13:21:40 -0700

    Pin docs and get them building in nix-shell --pure

--- nixpkgs/mkshell.nix
+++ nixpkgs/mkshell.nix
@@ -6,14 +6,15 @@ let
 
   pep562 = python37Packages.callPackage ./pep562 {};
   pymdown-extensions = python37Packages.callPackage ./pymdown-extensions { inherit pep562; };
-  mkdocs-minify-plugin = python37Packages.callPackage ./mkdocs-minify-plugin {};
-  mkdocs-material = python37Packages.callPackage ./mkdocs-material { inherit pymdown-extensions mkdocs-minify-plugin; };
+  mkdocs = python37Packages.callPackage ./mkdocs {};
+  mkdocs-minify-plugin = python37Packages.callPackage ./mkdocs-minify-plugin { inherit mkdocs; };
+  mkdocs-material = python37Packages.callPackage ./mkdocs-material { inherit pymdown-extensions mkdocs-minify-plugin mkdocs; };
 
-  beamPython = python37.withPackages (ps: [ pkgs.mkdocs mkdocs-material ps.sqlparse ps.pyyaml ]);
+  beamPython = python37.withPackages (ps: [ mkdocs mkdocs-material ps.sqlparse ps.pyyaml ]);
 
   docsRequirements = [ bash beamPython mkdocs pv sqlite
                        ncurses libcxx icu gcc mysql zlib openssl stack
-                       gnupg dos2unix vim pcre ];
+                       gnupg dos2unix vim pcre git ];
 in
   haskell.lib.buildStackProject {
     inherit ghc;
---
+++ nixpkgs/pinned.nix
@@ -0,0 +1,6 @@
+import ((import <nixpkgs> {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "5272327b81ed355bbed5659b8d303cf2979b6953";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+  }) {}
