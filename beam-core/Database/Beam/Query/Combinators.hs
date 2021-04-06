3b44460c3a67d74a2753ed8d8d2ea8f64f256c02
Rune K. Svendsen <runesvend@gmail.com>
2021-04-06 17:16:05 +0200

    tweak msg again

--- beam-core/Database/Beam/Query/Combinators.hs
+++ beam-core/Database/Beam/Query/Combinators.hs
@@ -658,7 +658,7 @@ class SqlOrderable be a | a -> be where
 instance SqlOrderable be (QOrd be s a) where
     makeSQLOrdering _ (QOrd x) = [x]
 
-instance TypeError ('Text "Unspecified sorting order" ':<>: 'Text "Use either 'asc_' or 'desc_' to specify sorting order") =>
+instance TypeError ('Text "Missing mandatory sorting order. Use either 'asc_' or 'desc_' to specify sorting order.") =>
     SqlOrderable be (QGenExpr ctx be s a) where
         makeSQLOrdering = error "SqlOrderable QGenExpr unreachable"
 
