d911c9addb0379fe161b78ed8b5d1f33179c279d
Ken Micklas <git@kmicklas.com>
2021-07-09 18:16:11 -0400

    Restore nix shell functionality with special old nixpkgs pin for docs

---
+++ nixpkgs/pinned-docs.nix
@@ -0,0 +1,4 @@
+import (builtins.fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/tarball/5272327b81ed355bbed5659b8d303cf2979b6953;
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+})
