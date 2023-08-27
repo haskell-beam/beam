76d1c21bfe53c4f8d6deac890d6406f9eec694c5
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-08-27 15:29:10 +0530

    refactor(standalone-instance): removes a now redundant standalone deriving instance
    * Removes the standalone deriving instance  for the "Generic"
      Typeclass for the 8-tuple data type
    
    * This instance might be missing in previous version of base but
      is now redundant with upgrade to GHC 9.2.7 with newer base version

--- beam-core/Database/Beam/Backend/SQL/Row.hs
+++ beam-core/Database/Beam/Backend/SQL/Row.hs
@@ -202,8 +202,6 @@ instance (FromBackendRow be x, FromBackendRow be SqlNull) => FromBackendRow be (
                       pure ()))
   valuesNeeded be _ = valuesNeeded be (Proxy @x)
 
-deriving instance Generic (a, b, c, d, e, f, g, h)
-
 instance (BeamBackend be, FromBackendRow be t) => FromBackendRow be (Tagged tag t) where
   fromBackendRow = Tagged <$> fromBackendRow
 
