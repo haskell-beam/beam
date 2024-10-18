223f2767e1b9f0668357647b48e1322697246aea
Travis Athougies <travis@athougies.net>
2024-10-18 18:10:10 +0000

    Implement support for inner CTEs in Postgres

--- flake.nix
+++ flake.nix
@@ -1,20 +1,27 @@
-let nixhash = import ./nix/nixhash.nix;
-in {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/${nixhash}";
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/85bcb95aa83be667e562e781e9d186c57a07d757";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { nixpkgs, flake-utils, self, ... }:
-    flake-utils.lib.forEachSystem (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
           beamLib = import ./nix/lib.nix { nixpkgs = pkgs; };
-          beamGhc = beamLib.makeBeamGhc ghc;
-      in {
-        devShells.default = beamGhc.shellFor {
-          packages = beamLib.beamPackageList;
-          nativeBuildInputs = [
+          beamGhc = beamLib.makeBeamGhc pkgs.haskellPackages;
+
+          shellWithNativeInputs = nis: beamGhc.shellFor {
+            packages = beamLib.beamPackageList;
+            nativeBuildInputs = nis;
+          };
+      in rec {
+        devShells.default = shellWithNativeInputs (with pkgs; [
             postgresql sqlite-interactive
-            cabal-install
-          ];
-        };
+            cabal-install haskell-language-server
+        ]);
+
+        devShells.build = shellWithNativeInputs []; # Shell only for building
+        devShells.test = shellWithNativeInputs (with pkgs; [ postgresql sqlite-interactive ]);
+
+        devShells.docs = shellWithNativeInputs (with pkgs; [ postgresql sqlite-interactive
+                                                             (python3.withPackages (ps: with ps; [ mkdocs mkdocs-material sqlparse ])) ]);
       });
 }
